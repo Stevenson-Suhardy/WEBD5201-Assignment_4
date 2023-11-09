973157e2b260ffc58334c5c233b42459783afc88
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-11-09 10:57:53 -0500

    Added styling for show employee page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'tasks#index'
+  root 'employees#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
