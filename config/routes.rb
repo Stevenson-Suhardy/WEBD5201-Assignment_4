3b37fe43a046b6ac3336fb3074203f9b1c30d034
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2023-11-10 01:32:23 -0500

    Replaced Employee with Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks
   resources :user_sessions, only: [:new, :create, :destroy]
-  resources :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_173404) do
-  create_table "employees", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "job_title"
-    t.integer "salary"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_061845) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
